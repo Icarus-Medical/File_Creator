cd6568ab212ef932920365ef69b6e10d5cc120f9
Josh <josh@icarusmedical.com>
2024-05-28 10:21:43 -0400

    Import files from builder quueue in Sandcastle

--- File_Creator.py
+++ File_Creator.py
@@ -10,12 +10,10 @@ des = app.activeProduct
 root = des.rootComponent
 api = Api.Api()
 
-def getOrder(orderId):
+def getOrder(data):
     #This is pulling all the data from the travelers, same as Icarus Add In with some added variables 
-    try: 
-        response = api.get(f"/api/order/{orderId}")
-        data = response.data
-
+    try:
+        id             = data["id"]
         patientName    = data["patientName"]
         model          = data["catalog"] #same setup as leg["name"]
         step           = data["lastJobEvent"] #if None traveler has not been started 
@@ -27,6 +25,7 @@ def getOrder(orderId):
         status         = data["status"] #open or pending
 
         return {
+            "id"                    : id,
             "patientName"           : patientName,
             "engraving"             : engraving if engraving is not None else "",
             "leg"                   : leg["name"] if leg is not None else "",
@@ -41,16 +40,7 @@ def getOrder(orderId):
             "status"                : status["name"] if status is not None else ""
         }
     except:
-        return {
-            "engraving"             : "",
-            "leg"                   : "",
-            "top_serial_number"     : "",
-            "bottom_serial_number"  : "",
-            "hanger"                : False,
-            "va"                    : False,    
-            "flexion"               : False,
-            "bilateral"             : False
-        }
+        return None
 
 def file_copy(fileName, orientation, a3):
     #This function grabs the proper starter file and creates a copy into the production folder 
@@ -97,67 +87,76 @@ def file_copy(fileName, orientation, a3):
     newFile.name = fileName 
 
 
-def file_exe(orderId, request):
+def file_exe(fileName, data, mesh):
     #the actual execution file, takes in order ID, grabs the traveler data
     #uses it to create the fileName string. That is then fed into the fileCopy function
     
     #Pulling relevant patient variables from the getOrder function
-    orientation    = request["leg"].lower()
-    patientName    = request["patientName"]
-    model          = request["catalog"] #this tells me if its Ascender, KAFO or A3.0
-    step           = request["lastJobEvent"] #if None, traveler is not started
-    travelerStatus = request["travelerStatus"]
+    orientation    = data["leg"].lower()
+    model          = data["catalog"] #this tells me if its Ascender, KAFO or A3.0
+    step           = data["lastJobEvent"] #if None, traveler is not started
+    travelerStatus = data["travelerStatus"]
 
     if model == 'Ascender - Custom' or model == 'Ascender 3 - Custom':
-    #split patientName into first and last
-        nameList = patientName.split()
-        firstName = nameList[0]
-        try:
-            lastName = nameList[1]
-        except:
-            lastName = ' '
-
         #Checking if the model is an ascender 3. If '3' is present as the 2nd word
         if model.split()[1] == "3":
             a3 = True
         else:
             a3 = False
-        #Pairing orientation with our shorthand naming convention
-        if orientation == 'left':
-            legName = 'L'
-        else:
-            legName = "R"
-        idName = str(orderId)
-
-        #Creating one string with our file naming convention, this is fed into the file_copy function
-        fileName = idName + "_" + lastName + "_" + firstName + "_" + legName
 
         #Check if traveler is Not Started and  not archived, execute file_copy
         if step is None and travelerStatus != "ARCHIVED":
             #app.log(str(orderId))
-            file_copy(fileName, orientation, a3)
+            file_copy(fileName, orientation, a3)    
 
-#i need to grab these automatically. Check if traveler is not started and if it is not archived, then create
+def importFiles():
+    # Get a list of all builder files ready for import
+    app.log("Requesting builder files")
+    filesResponse = api.get("/api/fusionFile/all")
 
-#Setting a range of order Ids to iterate through and create files for
-#This needs to be manually updated by the user (need to change this)
-upperBoundId = 13473
-lowerBoundId = 13455
-for i in range(lowerBoundId,upperBoundId+1):
-    request = getOrder(i)
+    # Make sure the API request was successful
+    if filesResponse.status != 200:
+        api.log(f"Could not get files. Response: {filesResponse.status} - {filesResponse.reason}")
+        return
 
-    #Checking to make sure order is open and not pending
-    try:
-        orderStatus = request["status"]
-        exe = True
-    except:
-        exe = False
+    data = filesResponse.data
 
-    if orderStatus == "Open" and exe:
+    # Loop through the list of builder files ready for import
+    for file in data:
+        fileLabel = f"{file['name']} ({file['id']}"
+
+        # Get the data for a specific builder file
+        app.log(f"Requesting data for file {fileLabel}")
+        fileResponse = api.get(f"/api/fusionFile/{file['id']}")
+
+        # Make sure the API request was successful
+        if fileResponse.status != 200:
+            app.log(f"Could not get file {fileLabel}). Response: {fileResponse.status} - {fileResponse.reason}")
+            continue
+
+        data = fileResponse.data
+
+        # Get the order data for the file
+        order = getOrder(data["order"])
+
+        # Make sure we were able to get order data
+        if order is None:
+            app.log(f"Could not get order data for file {fileLabel}")
+            continue
+
+        fileName = data["name"]
+        mesh = data["mesh"]
+
+        # Create a new Fusion file
         try:
-            file_exe(i, request)
+            # Check to make sure order is open
+            if order["status"] == "Open":
+                file_exe(fileName, order, mesh)
+
+                # File creation was successful. Remove from the list of builder files
+                app.log(f"Import successful. Removing data for file {fileLabel}")
+                api.post(f"/api/fusionFile/{file['id']}/delete", {})
         except:
-            app.log("File creation failed for order #" + str(i))
-
-
-#test
+            app.log(f"Import failed for file {fileLabel}")
+            
+importFiles()
